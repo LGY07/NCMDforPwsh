4194b51a8e8adbfeb66cf495a42acc5574f6dc56
UrsusFeline <84217988+LGY07@users.noreply.github.com>
2022-04-30 10:32:30 +0800

    Add files via upload

---
+++ ncmd.ps1
@@ -0,0 +1,16 @@
+$host.ui.RawUI.WindowTitle="Netease Cloud Music song Downloader"
+$TRUE_FALSE=$(Test-Path .\wget.exe)
+if( $TRUE_FALSE -eq "True" )
+{
+    Write-Output "Netease Cloud Music song Downloader"
+} else {
+    iwr -uri 'https://eternallybored.org/misc/wget/1.21.3/64/wget.exe' -out .\wget.exe
+}
+$SONGID = Read-Host "Please enter song ID"
+$FILENAME = $SONGID+".mp3"
+$DOWNLOAD ="url?id="+$SONGID+".mp3"
+$URL = "https://music.163.com/song/media/outer/url?id="+$FILENAME
+Write-Output "Downloading..."
+.\wget.exe $URL 2> NCMD.log
+Move-Item $DOWNLOAD $FILENAME
+Write-Output "Done!"
